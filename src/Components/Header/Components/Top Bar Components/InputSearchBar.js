7650febb8e163f65671e8e82ee87975a0bf3bd9d
andrey_mardash <andrey.mardash@gmail.com>
2020-10-04 00:09:05 +0300

    Created state obj in constructor in InputSearchBar.js file for further manipulations with text

--- src/Components/Header/Components/Top Bar Components/InputSearchBar.js
+++ src/Components/Header/Components/Top Bar Components/InputSearchBar.js
@@ -3,6 +3,19 @@ import React from "react";
 
 class InputSearchBar extends React.Component {
 
+  constructor(props){
+    super(props);
+
+    this.state = {
+      inputText: '',
+    }
+  }
+
+  changeInput(val){
+    console.log(val);
+    this.setState({inputText: val})
+  }
+
 
   render() {
 
@@ -10,7 +23,8 @@ class InputSearchBar extends React.Component {
         <div className="input-search-bar">
           <input type="text" name="w" id="sli_search_1" className="SearchBox"
                  placeholder="Search product or brand" required="required" spellCheck="false"
-                 autoComplete="off"/>
+                 value={this.state.value}
+                 autoComplete="off" onChange={(e) => this.changeInput(e.target.value)}/>
           <input type="text" className='InputButton' readOnly='readonly'/>
         </div>
     );
