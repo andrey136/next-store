53c89d71c71b7a7272646788658b2a2430473f86
andrey_mardash <andrey.mardash@gmail.com>
2020-10-06 21:54:46 +0300

    Footer Links Content Shortened the code by creating this.links object where I put all the text values

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,5 +1,8 @@
 import React from "react";
 import './footer.css';
+// Modules
+import FooterLinksContent from "./Footer Components/FooterLinksContent";
+// Pics
 import vk from '../../pics/vk.png';
 import twitter from '../../pics/twitter.png';
 import facebook from '../../pics/facebook.png';
@@ -13,106 +16,7 @@ class Footer extends React.Component {
 
     return (
       <footer class="PageFooter HomeFooter">
-        <section class="FooterLinks Content">
-          <div className="Inner clearfix">
-
-
-            <section className="LinkSection">
-              <div className="Title">Help</div>
-              <ul className="LinkList">
-                <li className="Item">
-                  <a href="">
-                    <span>My account</span>
-                  </a>
-                </li>
-
-                <li className="Item">
-                  <a href="">
-                    <span>Returns Information</span>
-                  </a>
-                </li>
-
-                <li className="Item">
-                  <a href="">
-                    <span>Shipping Information</span>
-                  </a>
-                </li>
-
-                <li className="Item">
-                  <a href="">
-                    <span>Contact Us</span>
-                  </a>
-                </li>
-
-                <li className="Item">
-                  <a href="">
-                    <span>Sitemap</span>
-                  </a>
-                </li>
-              </ul>
-            </section>
-
-            <section className="LinkSection">
-              <div className="Title">Privacy & Legal</div>
-              <ul className="LinkList">
-                <li className="Item">
-                  <a href="">
-                    <span>Privacy Policy</span>
-                  </a>
-                </li>
-
-                <li className="Item">
-                  <a href="">
-                    <span>Terms & Conditions</span>
-                  </a>
-                </li>
-              </ul>
-            </section>
-
-            <section className="LinkSection">
-              <div className="Title">Other services</div>
-              <ul className="LinkList">
-                <li className="Item">
-                  <a href="">
-                    <span>Next Mobile</span>
-                  </a>
-                </li>
-
-                <li className="Item">
-                  <a href="">
-                    <span>Media & Press</span>
-                  </a>
-                </li>
-
-                <li className="Item">
-                  <a href="">
-                    <span>The Company</span>
-                  </a>
-                </li>
-
-                <li className="Item">
-                  <a href="">
-                    <span>Careers@next</span>
-                  </a>
-                </li>
-
-                <li className="Item">
-                  <a href="">
-                    <span>Next Franchise Next Affiliates</span>
-                  </a>
-                </li>
-
-                <li className="Item">
-                  <a href="">
-                    <span>Next Affiliates</span>
-                  </a>
-                </li>
-              </ul>
-            </section>
-
-
-          </div>
-        </section>
+        <FooterLinksContent/>
         <section className="footerBottomBar Content">
           <div className="InnerFooter clearfix">
             <section className="Copyright"> © 2020 Next LLC. All Rights Reserved.</section>
