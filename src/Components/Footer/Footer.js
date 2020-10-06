4e5c6ef97f5fef76597c2bdb66d6086d8f17b0e3
andrey_mardash <andrey.mardash@gmail.com>
2020-10-06 21:20:34 +0300

    Footer Bottom Bar Added COPYRIGHT | QUICK LINKS(SOCIAL MEDIA) Added 6 pictures

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,5 +1,11 @@
 import React from "react";
-import './footer.css'
+import './footer.css';
+import vk from '../../pics/vk.png';
+import twitter from '../../pics/twitter.png';
+import facebook from '../../pics/facebook.png';
+import youtube from '../../pics/youtube.png';
+import pinterest from '../../pics/pinterest.png';
+import instagram from '../../pics/instagram.png'
 
 class Footer extends React.Component {
 
@@ -108,7 +114,17 @@ class Footer extends React.Component {
           </div>
         </section>
         <section className="footerBottomBar Content">
-
+          <div className="InnerFooter clearfix">
+            <section className="Copyright"> © 2020 Next LLC. All Rights Reserved.</section>
+            <ul className="LinkList">
+              <li className="Item"><a href=""><img src="" alt=""/><img src={vk} alt=""/></a></li>
+              <li className="Item"><a href=""><img src="" alt=""/><img src={facebook} alt=""/></a></li>
+              <li className="Item"><a href=""><img src="" alt=""/><img src={twitter} alt=""/></a></li>
+              <li className="Item"><a href=""><img src="" alt=""/><img src={instagram} alt=""/></a></li>
+              <li className="Item"><a href=""><img src="" alt=""/><img src={pinterest} alt=""/></a></li>
+              <li className="Item"><a href=""><img src="" alt=""/><img src={youtube} alt=""/></a></li>
+            </ul>
+          </div>
         </section>
       </footer>);
   }
