d6a28172b590223495b331cba83e0dab77db2698
andrey_mardash <andrey.mardash@gmail.com>
2020-10-07 16:54:35 +0300

    Updates

---
+++ precache-manifest.dccbcecb08a6a07779c9184eccdd2bc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91019705a9124d36dcd8b469862e67e6",
+    "url": "/next-store/index.html"
+  },
+  {
+    "revision": "d1d1946caf270038d12c",
+    "url": "/next-store/static/css/main.dc35b74c.chunk.css"
+  },
+  {
+    "revision": "34700a86b381f292f218",
+    "url": "/next-store/static/js/2.1cc2da82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/next-store/static/js/2.1cc2da82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1d1946caf270038d12c",
+    "url": "/next-store/static/js/main.4be33f2f.chunk.js"
+  },
+  {
+    "revision": "c2539a9086c94f46595e",
+    "url": "/next-store/static/js/runtime-main.7bf92f1d.js"
+  },
+  {
+    "revision": "35a4ea8d40bd9546a9295b75272a6427",
+    "url": "/next-store/static/media/small-flags-sprite.35a4ea8d.png"
+  }
+]);
