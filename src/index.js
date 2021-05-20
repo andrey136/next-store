ef53a5b00c791d8ca154f90166e5bf281d6fd801
andrey_mardash <andrey.mardash@gmail.com>
2021-05-20 21:18:18 +0300

    Added redux wrapper in index.js file

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Provider from "react-redux/lib/alternate-renderers";
+import configureStore from "./redux/store/store";
 
-ReactDOM.render(
+// ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={configureStore()}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
+
